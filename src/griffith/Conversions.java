28f28dd7fd91fb960c0ec88cf4be35c263609595
SD_Lab_3184481 <3225525769@qq.com>
2026-02-25 00:27:06 +0000

    git add Conversions.java git commit-m "feat:add method stubs in Conversions.java" git push

---
+++ src/griffith/Conversions.java
@@ -0,0 +1,22 @@
+package griffith;
+
+public class Conversions {
+    public double euroToDollar(double v){
+        return 0.0;
+
+    }
+    public double dollarToEuro(){
+
+    }
+    public int StringToInteger(){
+
+    }
+    public String integerToString(){
+
+    }
+    public String switchCase(){
+
+    }
+
+
+}
---
+++ src/griffith/ConversionsTest.java
@@ -0,0 +1,34 @@
+package griffith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ConversionsTest {
+    private final Conversions conversions = new Conversions();
+    private static final double ExchangRate = 1.08;
+
+    @org.junit.jupiter.api.Test
+    void testEuroToDollar() {
+        assertEquals(108.0,conversions.euroToDollar(100.0));
+        assertEquals(-108.0,conversions.euroToDollar(-100.0));
+        assertEquals(0.0,conversions.euroToDollar(0.0));
+
+
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void dollarToEuro() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void stringToInteger() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void integerToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void switchCase() {
+    }
+}
