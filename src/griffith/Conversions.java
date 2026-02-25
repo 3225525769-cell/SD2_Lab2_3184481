eef297b777ab9457680e9d6b4723926ea6837969
3225525769-cell <3225525769@qq.com>
2026-02-25 13:02:57 +0000

    git add Conversions.java git commit-m "feat:implement  stringToString method" git push

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -1,26 +1,31 @@
 package griffith;
 
 public class Conversions {
-    public double euroToDollar(double a){
-        return a*1.08;
+    public double euroToDollar(double a) {
+        return a * 1.08;
 
     }
-    public double dollarToEuro(double b){
 
-        return b/1.08;
+    public double dollarToEuro(double b) {
+
+        return b / 1.08;
     }
-    public int stringToInteger(String number){
+
+    public int stringToInteger(String number) {
 
         return Integer.parseInt(number);
     }
-    public String integerToString(){
+
+    public String integerToString() {
 
         return "";
     }
-    public String switchCase(){
+
+    public String switchCase() {
 
         return "";
     }
+}
 
 
 
